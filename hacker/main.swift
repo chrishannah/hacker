aa67d02812464e84ddc9ac32c3aba290376445ee
Christopher Hannah <christopher.hannah@me.com>
2017-03-27 10:21:30 +0100

    Initial Commit

---
+++ hacker/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  hacker
+//
+//  Created by Christopher Hannah on 27/03/2017.
+//  Copyright © 2017 Christopher Hannah. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
